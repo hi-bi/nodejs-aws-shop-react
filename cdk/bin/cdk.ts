903b0a9e3305ee8b6727ddbf7bf44225aa694afd
hi-bi <qds315@gmail.com>
2024-06-08 22:13:21 +0300

    feat: create and distribute site (bucket)

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkStack } from '../lib/cdk-stack';
+import { ShopSiteStack } from '../lib/cdk-stack';
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack', {
+new ShopSiteStack(app, "ShopSite-Stack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -19,3 +19,5 @@ new CdkStack(app, 'CdkStack', {
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+
+app.synth();
--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -2,11 +2,50 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class CdkStack extends cdk.Stack {
+export class ShopSiteStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    const cloudFrontOAI = new cdk.aws_cloudfront.OriginAccessIdentity(this, "ShopSite-OAI")
+
+    const siteBucket = new cdk.aws_s3.Bucket( this, "ShopSite-Bucket", {
+      bucketName: "bv-eu-north-1-rss-2",
+      websiteIndexDocument: "index.html",
+      publicReadAccess: false,
+      blockPublicAccess: cdk.aws_s3.BlockPublicAccess.BLOCK_ALL
+    } )
+
+    siteBucket.addToResourcePolicy(new cdk.aws_iam.PolicyStatement({
+      actions: ["S3:GetObject"],
+      resources: [siteBucket.arnForObjects("*")],
+      principals:[new cdk.aws_iam.CanonicalUserPrincipal(cloudFrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId)]
+    }))
+
+    const distribution = new cdk.aws_cloudfront.CloudFrontWebDistribution(this, "ShopSite-Distribution", {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: siteBucket,
+            originAccessIdentity: cloudFrontOAI
+          },
+          behaviors : [ 
+            {
+              isDefaultBehavior: true
+            }
+          ]
+        }
+      ]
+    })
+
+    new cdk.aws_s3_deployment.BucketDeployment(this, "SiteShop-Bucket-Deployment", {
+      sources: [cdk.aws_s3_deployment.Source.asset("../../dist")],
+      destinationBucket: siteBucket,
+      distribution: distribution,
+      distributionPaths: ["/*"]
+    })
+
 
     // example resource
     // const queue = new sqs.Queue(this, 'CdkQueue', {
