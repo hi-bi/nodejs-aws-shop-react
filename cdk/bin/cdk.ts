48aea5854f1fd2172bc0ae0dacc6fc31170dd6a5
hi-bi <qds315@gmail.com>
2024-06-12 09:17:39 +0300

    feat: OAC investigation

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -4,7 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import { ShopSiteStack } from '../lib/cdk-stack';
 
 const app = new cdk.App();
-new ShopSiteStack(app, "ShopSite-Stack", {
+new ShopSiteStack(app, "ShopSite7-Stack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
