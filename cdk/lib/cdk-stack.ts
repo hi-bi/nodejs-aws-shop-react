432f528193d467fc4c6107ce961df2fe657f77ea
hi-bi <qds315@gmail.com>
2024-06-13 18:22:20 +0300

    feat: initial

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,11 +1,19 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins'; // The CloudFront Origins CDK Library
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import * as origin from 'aws-cdk-lib/aws-cloudfront-origins';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as s3deployment from 'aws-cdk-lib/aws-s3-deployment';
-import { Construct, IConstruct } from 'constructs';
-import { IPolicy } from 'aws-cdk-lib/aws-iam';
+import { Construct } from 'constructs';
+import { API_PATHS, appName, taskNum } from './constants/cdk-constants';
+import { S3AccessPolicy } from './cdk-s3-policy';
+import { BEHAVIORS } from './config/cdk-ws-cache';
+import { ComparePolicies, Policies } from './cdk-cache-policy';
+import { IOrigin } from 'aws-cdk-lib/aws-cloudfront';
+import { REDIRECTS } from './config/cdk-ws-redirect';
+import { createRedirectFunction } from './functions/cdk-redirect';
+//import * as lambda from 'aws-cdk-lib/aws-lambda'; // The Lambda CDK Library !!!Only for 'us-east-1' region
+
 
 export class ShopSiteStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -13,173 +21,120 @@ export class ShopSiteStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-    const appName = "ShopSite7"
-    const appId = "227"
-
-    const siteBucket = new s3.Bucket( this, `${appName}-Bucket`, {
-      bucketName: `bv-eu-north-1-rss-${appId}`,
-      websiteIndexDocument: "index.html",
-      publicReadAccess: false,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    const siteBucket = new s3.Bucket( this, `${appName}-${taskNum}-S3`, {
+      bucketName: `bv-eu-north-1-rss-${taskNum}`,
+      //websiteIndexDocument: "index.html",     //commented, S3 is not httpOrigin and moved to the Distribution.defaultRootObject
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true
     } )
 
-    //const cloudFrontOAI = new cloudfront.OriginAccessIdentity(this, `${appName}-OAI`)
-
-/*    
-    const distribution = new cloudfront.CloudFrontWebDistribution(this, `${appName}-Distribution`, {
-      originConfigs: [
-        {
-          s3OriginSource: {
-            s3BucketSource: siteBucket,
-            originAccessIdentity: cloudFrontOAI
-          },
-          behaviors : [ 
-            {
-              isDefaultBehavior: true
-            }
-          ]
-        }
-      ]
-    })
-*/
-
-    const distribution = new cloudfront.Distribution(this, `${appName}-Distribution`, {
-      defaultBehavior: { 
-        origin: new origin.S3Origin(siteBucket),
-        //viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-        
-      },
-      defaultRootObject: "index.html",
+    const s3Origin = new origins.S3Origin(siteBucket, {
+      originAccessIdentity: new S3AccessPolicy(this, siteBucket).accessIdentity,
     });
-    const cfnDistribution = distribution.node.defaultChild as cloudfront.CfnDistribution;
 
-    const cfnOriginAccessControl = new cloudfront.CfnOriginAccessControl(this, `${appName}-OAC`, {
-      originAccessControlConfig: {
-        name: `oac-eu-north-1-rss-${appId}`,
-        originAccessControlOriginType: 's3',
-        signingBehavior: 'always',
-        signingProtocol: 'sigv4',
-    
-        // the properties below are optional
-        description: 'OAC to access the bucket',
-      },
-    })
+    const apiOrigin = new origins.HttpOrigin(API_PATHS.s3);
 
-    console.log('cfnOriginAccessControl.attrId: \n', cfnOriginAccessControl.getAtt('id'));
-
-
-
-    const siteOACBucketPolicy = new iam.PolicyStatement({
-      sid: 'AllowS3OACAccess',
-      effect: iam.Effect.ALLOW,
-      principals: [new iam.ServicePrincipal('cloudfront.amazonaws.com')],
-      actions: ["s3:GetObject"],
-      resources: [siteBucket.arnForObjects("*")],
-      conditions: {
-        StringEquals: {
-          "AWS:SourceArn": `arn:aws:cloudfront::767397818606:distribution/${distribution.distributionId}`,
-          //"AWS:SourceArn": `arn:aws:cloudfront::767397818606:distribution/${cfnOriginAccessControl.attrId}`,
-        },
-      },
-    }) 
-
-    siteBucket.addToResourcePolicy(siteOACBucketPolicy)
-
-    //// clean-up the OAI reference and associate the OAC with the cloudfront distribution 
-    // query the site bucket policy as a document
-    const bucketPolicy = siteBucket.policy
-    const bucketPolicyDocument = bucketPolicy?.document
-
-    // remove the CloudFront Origin Access Identity permission from the bucket policy
-      const Statement: Array<any> = [];
-      const bucketPolicyUpdatedJson = {'Version': '2012-10-17', 'Statement': Statement}
-
-    if (bucketPolicyDocument) {
-      //console.log('bucketPolicyDocument: ', bucketPolicyDocument);
-      //const bucketPolicyDocumentJson = JSON.parse(bucketPolicyDocument as unknown as string);
-      const bucketPolicyDocumentJson = bucketPolicyDocument.toJSON() as {Statement: Array<any>, Version: string};
-
-      //console.log('bucketPolicyDocumentJson: ', bucketPolicyDocumentJson);
-
-      // create an updated policy without the OAI reference
-      
-      const statement = bucketPolicyDocumentJson.Statement
-      //console.log('Statement: ', statement);
-      for(let i = 0; i < statement.length; i++) {
-        let principal = statement[i]?.Principal as { AWS: string };
-        //console.log('principal: ', principal);
-        if (!principal.AWS?.includes("Origin Access Identity")) {
-          const el = statement[i]
-          bucketPolicyUpdatedJson.Statement.push(el)
-
-        }
-      }
-      
+    const originsCreated: { [id: string]: IOrigin; } = {
+      "s3": s3Origin,
+      "api": apiOrigin,
     }
 
-    //console.log ('bucketPolicyUpdatedJson: ', bucketPolicyUpdatedJson);
-
-    // apply the updated bucket policy to the bucket
-    //console.log('siteBucket: ', siteBucket);
-    const bucketPolicyOverride = siteBucket.node.findChild("Policy").node.defaultChild
-    //console.log('siteBucket.Policy: ', bucketPolicyOverride);
-
-//    bucketPolicyOverride?.node.setContext('Properties.PolicyDocument', bucketPolicyUpdatedJson)
-
-    // remove the created OAI reference (S3 Origin property) for the distribution
-    //console.log('distribution: ', distribution);
-    const allDistributionProps = distribution.node.findAll()
-
-    //console.log('allDistributionProps: ', allDistributionProps);
-
-    //allDistributionProps.forEach(child => {
-    //  if (child.node.id == 'S3Origin') {
-    //    child.node.tryRemoveChild('Resource')
-    //  }
+    //const onlyDesktop = new lambda.Function(this, 'onlyDesktop', {
+    //  runtime: lambda.Runtime.NODEJS_16_X,
+    //  handler: 'cdk-onlydesktop.handler',                    // File name that contains our function
+    //  code: lambda.Code.fromAsset('./lib/functions'),    // Location of File
     //});
-
-    // associate the created OAC with the distribution
-    console.log('distribution.node.defaultChild: ', distribution.node.defaultChild)
-    const distributionProps = distribution.node.defaultChild;
-    //distributionProps?.node.setContext('Properties.DistributionConfig.Origins.0.S3OriginConfig.OriginAccessIdentity', '')
-    //distributionProps?.node.setContext("DistributionConfig.Origins.0.OriginAccessControlId", cfnOriginAccessControl.attrId)
-
-    //const cfnDistribution = distribution.node.defaultChild as cloudfront.CfnDistribution;
-    //console.log('cfnDistribution: \n', cfnDistribution);
     
-    //console.log('cfnDistribution.cfnOptions: \n', cfnDistribution.cfnOptions);
-    //console.log('cfnDistribution.cfnOptions: \n', cfnDistribution);
-    
-    console.log(' \n');
-    console.log('siteBucket.bucketDomainName: \n', siteBucket.bucketDomainName);
-    console.log('cfnDistribution.attrDomainName: \n', cfnDistribution.attrDomainName);
-    console.log('cfnDistribution.distributionConfig: \n', cfnDistribution.distributionConfig);
-    
-    //console.log('cfnDistribution.distributionConfig: \n', cfnDistribution.distributionConfig);
-   
-    //cfnDistribution.addDeletionOverride('DistributionConfig.Origins.Items.0.DomainName');
-    //cfnDistribution.addPropertyOverride('DistributionConfig.Origins.Items.0.DomainName', siteBucket.bucketDomainName);
-    //cfnDistribution.addDeletionOverride('DistributionConfig.Origins.Items.0.OriginAccessControlId');
-    cfnDistribution.addPropertyOverride('DistributionConfig.Origins.Items.0.OriginAccessControlId', cfnOriginAccessControl.getAtt('id') ); //.getAtt('id')
-    //cfnDistribution.addPropertyOverride('DistributionConfig.Origins.Items.0.OriginAccessControlId', '658327ea-f89d-4fab-a63d-7e88639e58f6' ); //.getAtt('id')
-    cfnDistribution.addPropertyOverride('DistributionConfig.Origins.Items.0.OriginAccessControlId_1', '658327ea-f89d-4fab-a63d-7e88639e58f6' ); //.getAtt('id')
-    cfnDistribution.addOverride('Properties.Test','Test_value');
-    
-    console.log(' \n');
-    console.log('siteBucket: \n', siteBucket.bucketDomainName);
-    console.log('cfnDistribution.attrDomainName: \n', cfnDistribution.attrDomainName);
-    console.log('cfnDistribution.distributionConfig: \n', cfnDistribution.distributionConfig);
-    //console.log('cfnDistribution.distributionConfig: \n', cfnDistribution.distributionConfig);
+    //const customOriginRequestPolicy = new cloudfront.OriginRequestPolicy(this, 'customOriginRequestPolicy', {
+    //  headerBehavior: cloudfront.OriginRequestHeaderBehavior.allowList('CloudFront-Is-Desktop-Viewer'),
+    //}); 
 
-    //console.log('cfnOriginAccessControl: \n', cfnOriginAccessControl)
+    
+    const distribution = new cloudfront.Distribution( this, `${appName}-${taskNum}-Distribution`, {
+      defaultBehavior: {
+        origin: s3Origin,
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+      },
+      defaultRootObject: "index.html",
+      
+      additionalBehaviors: {
+        'api/*': {
+          origin: apiOrigin,
+          allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
+          viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
 
-    new s3deployment.BucketDeployment(this, `${appName}-Bucket-Deployment`, {
+           // Add cache policy:
+          cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
+
+          // Add the origin request policy as such:
+          //originRequestPolicy: customOriginRequestPolicy,
+
+          // Add the Lambda@Edge function as such:
+          //edgeLambdas: [{
+          //  functionVersion: onlyDesktop.currentVersion,
+          //  eventType: cloudfront.LambdaEdgeEventType.ORIGIN_RESPONSE,  // Origin Response
+          //}],    
+        }
+      }
+ 
+    }
+    )
+
+    // Create a Policies list that will eventually contain all created Policies objects
+    const policies: Policies[] = [];
+
+    // Loop through each behavior defined in BEHAVIORS:
+    for (const behavior of BEHAVIORS) {
+      // Compare the behavior with previous policies
+      const checkPolicy = new ComparePolicies(this, behavior, policies);
+      
+      // Take the newly created policy
+      const policy = checkPolicy.finalPolicy;
+
+      // If behavior did not match any Policies in policies, push the new policy into policies.
+      if (checkPolicy.noMatch) {
+        policies.push({
+          policy: policy,
+          cacheProps: behavior,
+        })
+      }
+
+      // For each path in the behaviors.paths list create a new behavior in our distribution:
+      for (let path of behavior.paths) {
+        distribution.addBehavior(path,
+          originsCreated[behavior.origin],
+          {
+            cachePolicy: policy,
+          });
+      };
+
+    };
+
+    // Loop through the function parameters defined in REDIRECTS
+    for (const redirect of REDIRECTS) {
+
+      // For each Function Parameter, create a new CloudFront Function
+      const redirectFunction = new cloudfront.Function(this, "redirect-from-" + redirect.from + "-to-" + redirect.to, {
+        code: cloudfront.FunctionCode.fromInline(createRedirectFunction(redirect)),
+      });
+      
+      // Create a new behavior using the s3Origin and the from path defined in redirect.ts
+      distribution.addBehavior(redirect.from,
+        s3Origin,
+        {
+          functionAssociations: [{
+            function: redirectFunction,
+            eventType: cloudfront.FunctionEventType.VIEWER_REQUEST,
+          }],
+        });
+    }
+
+    new s3deployment.BucketDeployment(this, `${appName}-${taskNum}-S3-Deployment`, {
       sources: [s3deployment.Source.asset("../dist")],
       destinationBucket: siteBucket,
-      //distribution: distribution,
-      //distributionPaths: ["/*"]
+      distribution: distribution,
+      distributionPaths: ["/*"]
     })
 
 
